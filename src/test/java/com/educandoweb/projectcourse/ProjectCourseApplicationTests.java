2688b5f84b336df52b13d5d89e16f65ea80eea0e
Gustavo Rossi <toxic200203@gmail.com>
2023-08-23 20:36:02 -0300

    commit project

---
+++ src/test/java/com/educandoweb/projectcourse/ProjectCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.projectcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
