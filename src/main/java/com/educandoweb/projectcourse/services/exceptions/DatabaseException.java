cdd317b0dce172550eb1d1366a872756c9f6f3d5
RossiGu <gustavo.2017316329@gmail.com>
2023-08-26 12:06:50 -0300

    errors

---
+++ src/main/java/com/educandoweb/projectcourse/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.educandoweb.projectcourse.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
